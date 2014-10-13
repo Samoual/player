695d06562d76889a0e7016c6bf040c7c9be7d80d
samoual <samoual@gmail.com>
2014-10-14 01:13:06 +0300

    first setup
    Installed codigniter and set the devlopment enviroment

---
+++ core/ais/logs/log-2014-10-14.php
@@ -0,0 +1,36 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2014-10-14 00:08:35 --> Config Class Initialized
+DEBUG - 2014-10-14 00:08:35 --> Hooks Class Initialized
+DEBUG - 2014-10-14 00:08:35 --> Utf8 Class Initialized
+DEBUG - 2014-10-14 00:08:35 --> UTF-8 Support Enabled
+DEBUG - 2014-10-14 00:08:35 --> URI Class Initialized
+DEBUG - 2014-10-14 00:08:35 --> Router Class Initialized
+DEBUG - 2014-10-14 00:08:35 --> No URI present. Default controller set.
+DEBUG - 2014-10-14 00:08:35 --> Output Class Initialized
+DEBUG - 2014-10-14 00:08:35 --> Security Class Initialized
+DEBUG - 2014-10-14 00:08:35 --> Input Class Initialized
+DEBUG - 2014-10-14 00:08:35 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-10-14 00:08:35 --> Language Class Initialized
+DEBUG - 2014-10-14 00:08:35 --> Loader Class Initialized
+DEBUG - 2014-10-14 00:08:35 --> Controller Class Initialized
+DEBUG - 2014-10-14 00:08:35 --> File loaded: core/ais/views/welcome_message.php
+DEBUG - 2014-10-14 00:08:35 --> Final output sent to browser
+DEBUG - 2014-10-14 00:08:35 --> Total execution time: 0.1520
+DEBUG - 2014-10-14 00:08:40 --> Config Class Initialized
+DEBUG - 2014-10-14 00:08:40 --> Hooks Class Initialized
+DEBUG - 2014-10-14 00:08:40 --> Utf8 Class Initialized
+DEBUG - 2014-10-14 00:08:40 --> UTF-8 Support Enabled
+DEBUG - 2014-10-14 00:08:40 --> URI Class Initialized
+DEBUG - 2014-10-14 00:08:40 --> Router Class Initialized
+DEBUG - 2014-10-14 00:08:40 --> No URI present. Default controller set.
+DEBUG - 2014-10-14 00:08:40 --> Output Class Initialized
+DEBUG - 2014-10-14 00:08:40 --> Security Class Initialized
+DEBUG - 2014-10-14 00:08:40 --> Input Class Initialized
+DEBUG - 2014-10-14 00:08:40 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-10-14 00:08:40 --> Language Class Initialized
+DEBUG - 2014-10-14 00:08:40 --> Loader Class Initialized
+DEBUG - 2014-10-14 00:08:40 --> Controller Class Initialized
+DEBUG - 2014-10-14 00:08:40 --> File loaded: core/ais/views/welcome_message.php
+DEBUG - 2014-10-14 00:08:40 --> Final output sent to browser
+DEBUG - 2014-10-14 00:08:40 --> Total execution time: 0.0395
